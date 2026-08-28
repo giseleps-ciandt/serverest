7e1bf0e8c8e77d0d0005a9d4c683e813142748b3
Gisele Paola de Souza <paola.gisele@gmail.com>
2026-08-28 15:55:43 -0300

    chore: remove Firefox from test matrix due to Cypress incompatibility - use Chrome only

--- cypress.config.js
+++ cypress.config.js
@@ -22,19 +22,7 @@ module.exports = defineConfig({
       require('cypress-mochawesome-reporter/plugin')(on)
 
       on('before:browser:launch', (browser, launchArgs) => {
-        // Firefox-specific args for CI/CD environments
-        if (browser.name === 'firefox') {
-          launchArgs.args.push('-new-instance')
-          launchArgs.args.push('-no-remote')
-          launchArgs.args.push('-no-profile')
-          // Disable sandbox and other security features for CI/CD
-          if (process.env.CI) {
-            // Use a temporary profile to avoid permission issues
-            launchArgs.args.push('-profile')
-            launchArgs.args.push('/tmp/cypress-firefox-profile')
-          }
-        }
-        // Chrome-specific args for CI/CD environments
+        // Chrome-specific args for CI/CD
         if (browser.name === 'chrome' || browser.name === 'chromium') {
           launchArgs.args.push('--no-sandbox')
           launchArgs.args.push('--disable-gpu')
